eef9530a83a0d77e10ca2d78626b34cd07d325b8
lewfer <llewelyn@arcwave.co.uk>
2023-09-13 11:14:50 +0100

    update shell script

--- update_from_github.sh
+++ update_from_github.sh
@@ -1,7 +1,8 @@
-rm -r -f /home/pi/speaknshow/* 2> ~/errors
-rm -r -f /home/pi/speaknshow/.* 2>> ~/errors
-git clone https://https://github.com/lewfer/speaknshow /home/pi/speaknshow-temp -q
-#cp -r /home/pi/speaknshow-temp/* /home/pi/tcl/speaknshow/
-#rm -r -f /home/pi/speaknshow-temp/* 2>> ~/errors
-#rm -r -f /home/pi/speaknshow-temp/.* 2>> ~/errors
+rm -r -f /home/pi/speaknshow-temp/* 2> ~/errors
+rm -r -f /home/pi/speaknshow-temp/.* 2>> ~/errors
+git clone https://github.com/lewfer/speaknshow.git /home/pi/speaknshow-temp -q
+cp -r /home/pi/speaknshow-temp/* /home/pi/speaknshow/
+rm -r -f /home/pi/speaknshow-temp/* 2>> ~/errors
+rm -r -f /home/pi/speaknshow-temp/.* 2>> ~/errors
+chmod +x /home/pi/speaknshow/*.sh
 
